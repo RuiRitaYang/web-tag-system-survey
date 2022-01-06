271315e16052d27f8f854801d725e81acf380c88
Rui Yang <rui.rita.yang@gmail.com>
2022-01-05 21:24:43 -0600

    [General] Reconstruct the whole procedure. (Not using session)

--- app/routes.py
+++ app/routes.py
@@ -1,21 +1,31 @@
+import random
+
 from app import app
-from flask import render_template, request, session
+from flask import render_template, request
+
 
 @app.before_request
 def before_request_func():
   pass
 
+
 @app.route("/")
 def index():
-  if 'consent' in session and session['consent']:
-    return render_template('index.html', consented=True)
-  else:
-    session.pop('consent') if 'consent' in session else None
-    return render_template('index.html', consented=None)
+  return render_template('consent_form.html', consented=None)
 
 
 @app.route("/consented", methods=['GET', 'POST'])
 def consented():
   consent = request.form.get('consent')
-  session['consent'] = (consent == 'y')
-  return render_template('index.html', consented=session['consent'])
+  if consent == 'y':
+    return render_template('background.html')
+  else:
+    return render_template('finish.html', finished=False)
+
+
+@app.route("/tag", methods=['GET', 'POST'])
+def tag():
+  scenario_ids = random.sample(range(1, 20), 2)
+  # TODO: stored in database
+  # TODO: grab related routine information
+  return render_template('tagging.html')
