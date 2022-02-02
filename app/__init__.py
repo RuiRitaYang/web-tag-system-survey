84823b99374493a613e3094584e12f756d1a83d7
Rui Yang <rui.rita.yang@gmail.com>
2022-02-02 11:49:11 -0600

    [deploy] tweaked app for heroku

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 # Add Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
 # Posgres DB
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ruiyang:123654@localhost/survey_test'
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://vjehzfgxguljlq:1cbae71db7a73531e2f9bbfe0193d4461ab6a3d7ac08d43fd645393f6379436f@ec2-44-199-111-161.compute-1.amazonaws.com:5432/d3b1k5jus1if16'
 # MySQL DB
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:11111111@localhost/test_users'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
